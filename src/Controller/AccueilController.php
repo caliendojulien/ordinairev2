adc0a5a7afd790e6daba148d83e24f03d98fa9f7
Thomas Fischer <92039475+thomas-c-p-fischer@users.noreply.github.com>
2022-08-17 15:41:09 +0200

    ROLE et NAV et ajout
    accés à certain lien selon le ROLE utilisateur
    ROLE_ADMIN accés à affichage
    ROLE_SUPER_ADMIN accés à gestion
    Barre de nav disponible sur toutes les pages twig
    ajout = possibilité pour le SUPER_ADMIN d'ajouter des personnels et formations

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,13 +2,15 @@
 
 namespace App\Controller;
 
-use App\Entity\Reservation;
+use App\Form\AjouterFType;
+use App\Form\AjouterType;
 use App\Repository\ReservationRepository;
 use App\Entity\Formation;
 use App\Entity\Utilisateur;
 use App\Repository\FormationRepository;
 use App\Repository\UtilisateurRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -24,7 +26,7 @@ class AccueilController extends AbstractController
     }
 
     #[Route('/affichage', name: '_affichage')]
-    public function affichage(Request $request,EntityManagerInterface $entityManager, ReservationRepository $repository): Response
+    public function affichage(Request $request, EntityManagerInterface $entityManager, ReservationRepository $repository): Response
     {
         $dateCalendrier = $request->get("calendrier");
         $cptMidi = $repository->count(['midi' => 1, 'date' => new \DateTime($dateCalendrier)]);
@@ -37,11 +39,12 @@ class AccueilController extends AbstractController
                 'cptSoir' => $cptSoir
             ]
         );
-        
+    }
+
     #[Route('/liste', '_listeUtilisateursFormations')]
     public function listeUtilisateurFormation(
-        UtilisateurRepository  $utilisateurRepository,
-        FormationRepository    $formationRepository
+        UtilisateurRepository $utilisateurRepository,
+        FormationRepository   $formationRepository
     ): Response
     {
         $listeUtilisateurs = $utilisateurRepository->findAll();
@@ -64,11 +67,41 @@ class AccueilController extends AbstractController
     #[Route('/supprimer/f/{formation}', '_supprimerF')]
     public function supprimerFormation(
         EntityManagerInterface $em,
-        Formation            $formation,
+        Formation              $formation,
     ): Response
     {
         $em->remove($formation);
         $em->flush();
         return $this->redirectToRoute('accueil_listeUtilisateursFormations');
+    }
+
+    #[Route('/ajouter', '_ajouter')]
+    public function ajouterUtilisateurFormation(
+        Request                $request,
+        EntityManagerInterface $em
+    ): Response
+    {
+        $utilisateur = new Utilisateur();
+        $formation = new Formation();
+        $utilisateurForm = $this->createForm(AjouterType::class, $utilisateur);
+        $formationForm = $this->createForm(AjouterFType::class, $formation);
+        $utilisateurForm->handleRequest($request);
+        $formationForm->handleRequest($request);
+
+        if ($utilisateurForm->isSubmitted() && $utilisateurForm->isValid()) {
+            $utilisateur->setRoles(['ROLE_USER']);
+            $em->persist($utilisateur);
+            $em->flush();
+            return $this->redirectToRoute('accueil_ajouter');
+        }
+
+        if ($formationForm->isSubmitted() && $formationForm->isValid()) {
+            $em->persist($formation);
+            $em->flush();
+            return $this->redirectToRoute('accueil_ajouter');
+        }
+
+        return $this->renderForm('ajouter/ajouter.html.twig',
+            compact('utilisateurForm', 'formationForm'));
     }
 }
