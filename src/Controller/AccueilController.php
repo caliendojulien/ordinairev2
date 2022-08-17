de77b3eedf683c51055d931986ced767b1c918e9
Sébastien Dijoux <sebastien.dijoux@outlook.fr>
2022-08-17 10:01:37 +0200

    Création de la page login

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,15 +2,38 @@
 
 namespace App\Controller;
 
+use App\Entity\Reservation;
+use App\Repository\ReservationRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/', name: 'accueil')]
 class AccueilController extends AbstractController
 {
-    #[Route('/', name: 'accueil_index')]
+    #[Route('/', name: '_index')]
     public function index(): Response
     {
         return $this->render('base.html.twig');
     }
+
+    #[Route('/affichage', name: '_affichage')]
+    public function affichage(Request $request,EntityManagerInterface $entityManager, ReservationRepository $repository): Response
+    {
+        $dateCalendrier = $request->get("calendrier");
+        $cptMidi = $repository->count(['midi' => 1, 'date' => new \DateTime($dateCalendrier)]);
+        $cptSoir = $repository->count(['soir' => 1, 'date' => new \DateTime($dateCalendrier)]);
+
+
+        return $this->render('affichageReservation.html.twig', [
+                'cptMidi' => $cptMidi,
+                'dateCalendrier' => $dateCalendrier,
+                'cptSoir' => $cptSoir
+            ]
+        );
+    }
+
+
 }
