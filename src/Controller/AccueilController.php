4cdc2a9affcb2f02b2c1a823ad3ee73cd541699c
Thomas Fischer <92039475+thomas-c-p-fischer@users.noreply.github.com>
2022-08-18 09:52:09 +0200

    commentaires
    ajout de commentaires sur les parties que j'ai réalisé
    pour mieux comprendre les différentes étapes

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -16,18 +16,32 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Ce controller contient les function de la page d'accueil, l'affichage des effectif mangeant,
+ * la page d'affichage des personnels et stagiaires, et la page pour ajouter personnels et formations
+ */
 #[Route('/', name: 'accueil')]
 class AccueilController extends AbstractController
 {
+    /**
+     * Cette fonction est celle de la page d'accueil (base.html.twig) elle renvoie simplement sur la page
+     */
     #[Route('/', name: '_index')]
     public function index(): Response
     {
         return $this->render('base.html.twig');
     }
 
+    /**
+     * Cette fonction fait appelle à des requètes qui vont compté en base de donnée les mangeants, pour une date donnée,
+     * le midi et le soir, et elle renvoie sur une page accessible seulement aux utilisateur ayant pour rôle ADMIN
+     */
     #[IsGranted("ROLE_ADMIN")]
     #[Route('/affichage', name: '_affichage')]
-    public function affichage(Request $request, EntityManagerInterface $entityManager, ReservationRepository $repository): Response
+    public function affichage(
+        Request                $request,
+        ReservationRepository  $repository
+    ): Response
     {
         $dateCalendrier = $request->get("calendrier");
         $cptMidi = $repository->count(['midi' => 1, 'date' => new \DateTime($dateCalendrier)]);
@@ -42,6 +56,10 @@ class AccueilController extends AbstractController
         );
     }
 
+    /**
+     * Cette fonction fait appelle à des requètes qui permettent d'afficher tout les utilisateurs et formations.
+     * De plus la page qu'elle renvoie n'est accessible que par les utilisateurs ayant pour rôle SUPER_ADMIN
+     */
     #[IsGranted("ROLE_SUPER_ADMIN")]
     #[Route('/liste', '_listeUtilisateursFormations')]
     public function listeUtilisateurFormation(
@@ -55,6 +73,10 @@ class AccueilController extends AbstractController
             compact('listeUtilisateurs', 'listeFormations'));
     }
 
+    /**
+     * Cette fonction fait appelle à des requètes qui permettent de supprimer un utilisateur.
+     * Elle renvoie sur la même page que la function précedente et n'est accessible que pour le rôle SUPER_ADMIN
+     */
     #[IsGranted("ROLE_SUPER_ADMIN")]
     #[Route('/supprimer/u/{utilisateur}', '_supprimerU')]
     public function supprimerUtilisateur(
@@ -67,6 +89,10 @@ class AccueilController extends AbstractController
         return $this->redirectToRoute('accueil_listeUtilisateursFormations');
     }
 
+    /**
+     * Cette fonction fait appelle à des requètes qui permettent de supprimer une formation.
+     * Elle renvoie sur la même page que la function précedente et n'est accessible que pour le rôle SUPER_ADMIN
+     */
     #[IsGranted("ROLE_SUPER_ADMIN")]
     #[Route('/supprimer/f/{formation}', '_supprimerF')]
     public function supprimerFormation(
@@ -79,6 +105,10 @@ class AccueilController extends AbstractController
         return $this->redirectToRoute('accueil_listeUtilisateursFormations');
     }
 
+    /**
+     * Cette fonction fait appelle à des requètes qui permettent l'ajout d'utilisateurs et de formations en BDD.
+     * Elle renvoie sur une nouvelle page qui elle aussi n'est accessible que par le rôle SUPER_ADMIN
+     */
     #[IsGranted("ROLE_SUPER_ADMIN")]
     #[Route('/ajouter', '_ajouter')]
     public function ajouterUtilisateurFormation(
